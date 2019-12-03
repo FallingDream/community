7b839be9f549332c70a25bac22377c742a018cc7
wang <1937568025@qq.com>
2019-12-03 20:32:35 +0800

    update_12_03

---
+++ src/main/java/com/wang/community/controller/AuthorizeController.java
@@ -0,0 +1,32 @@
+package com.wang.community.controller;
+
+import com.wang.community.dto.AccessTokenDTO;
+import com.wang.community.dto.GithubUser;
+import com.wang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code")String code,
+                           @RequestParam(name = "state")String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_id("0fa1040a492a78921872");
+        accessTokenDTO.setClient_secret("118f52b4d9f587c3cf090a6ceac1a2758e7918db");
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+
+        return "index";
+    }
+}
---
+++ src/main/java/com/wang/community/dto/GithubUser.java
@@ -0,0 +1,31 @@
+package com.wang.community.dto;
+
+public class GithubUser {
+    private String name;
+    private long id;
+    private String boi;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBoi() {
+        return boi;
+    }
+
+    public void setBoi(String boi) {
+        this.boi = boi;
+    }
+}
